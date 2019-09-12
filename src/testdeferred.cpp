2ae26e7d80bd9e1143f0ba9b1b38b6900a99d2cf
Sparke2 <ligreat875@gmail.com>
2019-09-12 03:57:44 +0300

    Update testdeferred.cpp

--- src/testdeferred.cpp
+++ src/testdeferred.cpp
@@ -1,24 +1,34 @@
 #pragma once
 
-#include <testdeferred.hpp>
-
+#include <eosiolib/eosio.hpp>
+#include <eosiolib/system.hpp>
+#include <eosiolib/transaction.hpp>
 
 using namespace eosio;
 
-class [[eosio::contract("testdeferred")]] hello : public contract {
+class [[eosio::contract("testdeferred")]] testdeferred : public contract {
   public:
       using contract::contract;
 
 [[eosio::action]]
-void setupdate(eosio::name from, eosio::name payer, uint64_t id, uint64_t delay, uint64_t tx_id)
+void callupdate(eosio::name from, eosio::name payer, uint64_t id, uint64_t delay)
+{
+     setupdate(from, payer, id, delay);
+}
+
+[[eosio::action]]
+void setupdate(eosio::name from, eosio::name payer, uint64_t id, uint64_t delay)
 {
      eosio::transaction t{};
      t.actions.emplace_back(
-          eosio::permission_level(get_self(), "active"_n),
-          get_self(),
+          eosio::permission_level(_self, "active"_n),
+          _self,
           "setupdate"_n,
-          std::make_tuple(id));
+          std::make_tuple(from, payer, id, delay));
      t.delay_sec = delay;
-     t.send(tx_id, get_self(), true);
+     t.send(now(), _self, true);
 }
 };
+
+
+EOSIO_DISPATCH( testdeferred, (setupdate)(callupdate))
